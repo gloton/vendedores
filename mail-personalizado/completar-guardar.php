fe815ae1cbf7f1475bb7a11f63ccda9cb089e0ad
Gloton <gloton@gmail.com>
2013-06-21 09:42:55 -0400

    Muestra detalle producto elegidos en la pagina que hace el proceso con la conexion a la base de datos.

--- mail-personalizado/completar-guardar.php
+++ mail-personalizado/completar-guardar.php
@@ -11,19 +11,13 @@ if (($nro_productos > 3) && ($nro_productos < 1)) {
 	//devolver a la pagina creacion con orden desplegar error
 	;
 }
-$i = 0;
 foreach ($productos as $indice => $valor){
 	
-	//echo $indice . " <br />" ;
 	if ($indice != "nombre") {
 		$sql_detalle = "SELECT detalle FROM productos_mail WHERE id_produto=$indice";
 		$query_detalle = mysql_query($sql_detalle, Conectar::con()) or die("No se pudo ejecutar consulta");			
 		$detalles["detalle"] = mysql_fetch_array($query_detalle);
-		
-		/* var_dump($detalles["detalle"][0]);
-		exit(); */
 	}
-	//var_dump($detalles["detalle"][0]);
 	echo $detalles["detalle"][0];
 }
 
