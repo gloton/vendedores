ea3008e8e24b69841a24fbf50b9fb8897065d938
Gloton <gloton@gmail.com>
2013-06-25 10:16:21 -0400

    Correccion alerta Para que funcionara la alerta original, estaba usando jquery 1.8.1 pero con esto dejaba de mostrarse los botones, la solucion  fue cambiarlo a jquery 1.5.1, pero dejaba de funcionar la alerta, es decir, no se cerraba la alerta al presionarle la cruz. Por lo que decidi dejarl jquery version 1.5.1 y cerrarlo manualmente, esto lo hice en el archivo script.js

--- mail-personalizado/js/script.js
+++ mail-personalizado/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-	
 	$('input[type=checkbox]').tzCheckbox({labels:['Habilidado','Deshabilitado']});
+    $('.close').click(function() {
+        //instrucciones
+        $('.container').css("display","none");
+	});
 });
